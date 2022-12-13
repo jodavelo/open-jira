fc81afee2be8cdccb08fd1c4ff33488cd1c0daba
José Daniel Vergara Lozano <josedani0630@hotmail.com>
2022-12-13 08:45:25 -0500

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@mui/material';
+import { NextPage } from 'next'
+import { Layout } from '../components/layouts';
+
+const HomePage: NextPage =() => {
+    return (
+        <Layout>
+            <Typography variant='h1' color='primary'>Hola mundo</Typography>
+        </Layout>
+    )
+}
+
+export default HomePage;
